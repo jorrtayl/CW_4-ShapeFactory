4db3b8f646079418cd3988f6c24027f13877289e
Jordan Taylor <jorrtayl@ut.utm.edu>
2021-02-04 05:31:27 -0600

    Got the program working.

--- CW 4 - ShapeFactory/Program.cs
+++ CW 4 - ShapeFactory/Program.cs
@@ -6,10 +6,82 @@ using System.Threading.Tasks;
 
 namespace CW_4___ShapeFactory
 {
+    class ShapeFactory
+    {
+        public IGeometricShape getShape(ShapeType type)
+        {
+            switch (type)
+            {
+                case ShapeType.LINE:
+                    return new Line();
+                case ShapeType.CIRCLE:
+
+                    return new Circle();
+                case ShapeType.RECTANGLE:
+
+                    return new Rectangle();
+                default:
+                    return null;
+            }
+        }
+    }
+
+    enum ShapeType 
+    { 
+        LINE, CIRCLE, RECTANGLE, TRIANGLE 
+    };
+    
+    interface IGeometricShape
+    {
+        void draw();
+    }
+
+    public class Line : IGeometricShape
+    {
+        string line = "Line";
+        public void draw()
+        {
+            Console.WriteLine(line + " has been drawn.");
+        }
+    }
+
+    public class Circle : IGeometricShape
+    {
+        string line = "Circle";
+        public void draw()
+        {
+            Console.WriteLine(line + " has been drawn.");
+        }
+    }
+
+    public class Rectangle : IGeometricShape
+    {
+        string line = "Rectangle";
+        public void draw()
+        {
+            Console.WriteLine(line + " has been drawn.");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            ShapeFactory shapeFactory = new ShapeFactory();
+
+            foreach (ShapeType shape in Enum.GetValues(typeof(ShapeType)))
+            {
+                var e = shapeFactory.getShape(shape);
+
+                if (e == null)
+                {
+                    Console.WriteLine(shape.ToString() + " is not implemented.");
+                }
+                else
+                {
+                    e.draw();
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
